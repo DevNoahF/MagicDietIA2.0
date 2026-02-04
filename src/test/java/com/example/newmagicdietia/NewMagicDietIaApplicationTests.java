701195f862a5f67d886af3094075d8ea9537b70c
unknown <noahvf16@outlook.com>
2026-02-04 11:59:05 -0300

    first commit

---
+++ src/test/java/com/example/newmagicdietia/NewMagicDietIaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newmagicdietia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewMagicDietIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
